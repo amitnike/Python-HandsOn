e45b4a7669a8f5ff3f38ba23630d49c458ebd62f
Amit <amit.naikwadi84@gmail.com>
2026-02-14 22:06:57 +0530

    handle none value in dictionary

--- 13-listOfDict.py
+++ 13-listOfDict.py
@@ -2,9 +2,10 @@ students =[
     {"name":"amit","add":"pune","job":"hsbc"},
     {"name":"ayansh","add":"mumbai","job":"infy"},
     {"name":"supriya","add":"hadapsar","job":"tcs"},
+    # if we want to add a student without job then we can assign None to the job key
     {"name":"test","add":"kharadi","job":None}
 ]
 
 
 for student in students:
-    print(student["name"],student["add"],student["job"],sep=",")
+    print(student["name"],student["add"],student["job"],sep=", ")
